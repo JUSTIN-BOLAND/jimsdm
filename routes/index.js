110ee733ab7e7be5dd4f59bc03bb9795767a2b8b
jim syyap <jimsyyap@gmail.com>
2014-10-14 20:49:02 +1300

    init before heroku

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. 
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/usedcardealer', function(req, res) {
+    res.render('usedcardealer', { title: 'Marketing Campaign For Used Car Dealer'});
+});
+*/
+
+module.exports = router;
