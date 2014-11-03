c17c32848229df2709d2ba07e0ab6400986f8bb4
jim syyap <jimsyyap@gmail.com>
2014-11-04 07:40:39 +1300

    add bucketlist.jade

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 // ROuTING
 
+//save html iriscouch, use mikael/request to pipe
+/*
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Pay Per Click Advertising, convertMost.com' });
+    var url = 'https://jims.iriscouch.com/serve_jimsdm/e337624277e365c49eb177668f0337e5/couch_request.htm'
+    request.get(url).pipe(res);
+});
+*/
+
+router.get('/bucketlist', function(req, res) {
+    res.render('bucketlist', {
+        title: 'Bucket List - Jim Syyap' });
+});
+
+router.get('/realestate_adv', function(req, res) {
+    res.render('realestate_adv', {
+        title: 'How To Get An Unfair But Legal Advantage When Listing And Selling Real Estate' });
+});
+
+router.get('/', function(req, res) {
+    res.render('index', { 
+        title: 'Pay Per Click Advertising, convertMost.com' });
 });
 
 router.get('/cinema', function(req, res) {
